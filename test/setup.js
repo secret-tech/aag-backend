909ff3f5d86f715d76a783d71eb496e754fef644
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-10-17 11:52:47 +0300

    Facebook login

--- test/setup.js
+++ test/setup.js
@@ -1,6 +1,6 @@
 import { EventEmitter } from 'events'
-import MongodbMemoryServer from 'mongodb-memory-server'
 import mongoose from '../src/services/mongoose'
+import { mongo } from '../src/config'
 
 EventEmitter.defaultMaxListeners = Infinity
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
@@ -24,9 +24,7 @@ global.parseFloat = parseFloat
 let mongoServer
 
 beforeAll(async () => {
-  mongoServer = new MongodbMemoryServer()
-  const mongoUri = await mongoServer.getConnectionString()
-  await mongoose.connect(mongoUri, (err) => {
+  await mongoose.connect(mongo.uri, (err) => {
     if (err) console.error(err)
   })
 })
