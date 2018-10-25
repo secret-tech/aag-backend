706abb806bd9516223171372d279bd9b57ea4d25
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-10-25 17:43:49 +0300

    socket.io

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import * as socketio from 'socket.io';
 
 const app = express(apiRoot, api)
 const server = http.createServer(app)
-const io = socketio(server);
+const io = socketio.listen(server);
 const sockets = {};
 const sock = io.of('/')
 
@@ -18,21 +18,22 @@ setImmediate(() => {
   server.listen(port, ip, () => {
     console.log('Express server listening on http://%s:%d, in %s mode', ip, port, env)
   })
-  sock.on('connection', (socket) => {
-    console.log('Socket connection');
-    socket.on('init', (userId) => {
-      sockets[userId.senderId] = socket;
-    });
-    socket.on('message', (message) => {
-      if (sockets[message.receiverId]) {
-        sockets[message.receiverId].emit('message', message);
-      }
-      /* handler for creating message */
-    });
-    socket.on('disconnect', (userId) => {
-      delete sockets[userId.senderId];
-    });
-  })
+})
+
+sock.on('connection', (socket) => {
+  console.log('Socket connection');
+  socket.on('init', (userId) => {
+    sockets[userId.senderId] = socket;
+  });
+  socket.on('message', (message) => {
+    if (sockets[message.receiverId]) {
+      sockets[message.receiverId].emit('message', message);
+    }
+    /* handler for creating message */
+  });
+  socket.on('disconnect', (userId) => {
+    delete sockets[userId.senderId];
+  });
 })
 
 export default app
