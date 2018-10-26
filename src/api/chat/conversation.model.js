420c0c6d1c022bf51fe582617f4f7fbce53e4e13
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-10-26 20:49:31 +0300

    Conv

--- src/api/chat/conversation.model.js
+++ src/api/chat/conversation.model.js
@@ -6,7 +6,7 @@ const conversationSchema = new Schema({
     userOne: { type: Schema.Types.ObjectId, ref: 'User' },
     userTwo: { type: Schema.Types.ObjectId, ref: 'User' },
     messages: [{
-        type: mongoose.Schema.Types.ObjectId,
+        type: String,
         ref: 'Message',
   }]
 })
