5ec5d68424e26809bd6f1c03e3bc9879199ae024
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-10-26 13:48:06 +0300

    Return the last fkn message

--- src/api/chat/controller.js
+++ src/api/chat/controller.js
@@ -8,8 +8,12 @@ export const listConversations = ({body, params, user}, res, next) => {
         .populate({path: 'conversations', populate: { path: 'userOne' }})
         .populate({path: 'conversations', populate: { path: 'userTwo' }})
         .then((user) => {
-            const conversations = user.conversations.map((conversation) => {
+            const conversations = user.conversations.map(async (conversation) => {
                 const friend = user._id.toString() === conversation.userOne._id.toString() ? conversation.userTwo : conversation.userOne;
+                if (conversation.messages.length > 0)
+                    conversation.messages  = [await Message.findById(conversation.messages[conversation.messages.length - 1])]
+                else 
+                    conversation.messages = []
                 return {
                     _id: conversation._id,
                     messages: conversation.messages,
