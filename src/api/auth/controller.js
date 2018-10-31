2130fa593e2c5817de9ad84cc1e647662bfab90b
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-11-01 00:18:08 +0300

    Remove registerFacebook endpoint

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -2,8 +2,10 @@ import { sign } from '../../services/jwt'
 import { success } from '../../services/response/'
 import User from '../user/model'
 
-export const login = (req, res, next) => {
-  console.log("Req: ", req.body);
+export const login = async (req, res, next) => {
+  req.user.services.oneSignal = req.body.playerId
+  await req.user.save()
+  console.log("Saved user: ", req.user)
   sign(req.user.id)
     .then((token) => ({ token, user: req.user.view(true) }))
     .then(success(res, 201))
