19942dd5e580e01b087f37d7b7ca2daddd016118
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-11-01 18:50:57 +0300

    Save only not empty OneSignal playerId

--- src/api/auth/controller.js
+++ src/api/auth/controller.js
@@ -3,8 +3,10 @@ import { success } from '../../services/response/'
 import User from '../user/model'
 
 export const login = async (req, res, next) => {
-  req.user.services.oneSignal = req.body.playerId
-  await req.user.save()
+  if(req.body.playerId && req.body.playerId !== '') {
+    req.user.services.oneSignal = req.body.playerId
+    await req.user.save()
+  }
   sign(req.user.id)
     .then((token) => ({ token, user: req.user.view(true) }))
     .then(success(res, 201))
