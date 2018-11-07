322f1eedf538c4a5412eae99cf1c4bb2a5d57e23
Andrey Degtyaruk <hlogeon1@gmail.com>
2018-11-07 12:46:30 +0300

    Add 'Please rare %username%' notification

---
+++ Dockerfile.worker
@@ -0,0 +1,2 @@
+FROM node:8.12-alpine
+WORKDIR '/var/www/app'
---
+++ src/notification.worker.js
@@ -0,0 +1,16 @@
+import { redis, oneSignal } from './config'
+import * as Queue from 'bull'
+
+const oneSignalClient = new Client({
+    userAuthKey: oneSignal.userKey,
+    app: { appAuthKey: oneSignal.apiKey, appId: oneSignal.appId }
+})
+const notificationQueue = new Queue('rating-notifications', redis.url)
+
+notificationQueue.process(async (job, data) => {
+    await oneSignalClient.sendNotification(data)
+});
+
+notificationQueue.on('completed', job => {
+    console.log('Job with id ' + job.id + ' has been completed');
+})
